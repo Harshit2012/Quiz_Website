3067415f67acb7cf6b42f55196b40c145aa40035
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2023-09-10 14:04:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+//qna1
+function clickcone(){
+    document.getElementById("correctanswerone").style.backgroundColor = "green";
+    alert("Correct Answer!!!")
+}
+
+function clickwone(){
+    document.getElementById("wronganswerone").style.backgroundColor = "red";
+    alert("Wrong Answer!!!")
+}
+
+//qna2
+function clickctwo(){
+    document.getElementById("correctanswertwo").style.backgroundColor = "green";
+    alert("Correct Answer!!!")
+}
+
+function clickwtwo(){
+    document.getElementById("wronganswertwo").style.backgroundColor = "red";
+    alert("Wrong Answer!!!")
+}
+
+//qna3
+function clickcthree(){
+    document.getElementById("correctanswerthree").style.backgroundColor = "green";
+    alert("Correct Answer!!!")
+}
+
+function clickwthree(){
+    document.getElementById("wronganswerthree").style.backgroundColor = "red";
+    alert("Wrong Answer!!!")
+}
+
+//qna4
+function clickcfour(){
+    document.getElementById("correctanswerfour").style.backgroundColor = "green";
+    alert("Correct Answer!!!")
+}
+
+function clickwfour(){
+    document.getElementById("wronganswerfour").style.backgroundColor = "red";
+    alert("Wrong Answer!!!")
+}
+
+//qna5
+function clickcfive(){
+    document.getElementById("correctanswerfive").style.backgroundColor = "green";
+    alert("Correct Answer!!!")
+}
+
+function clickwfive(){
+    document.getElementById("wronganswerfive").style.backgroundColor = "red";
+    alert("Wrong Answer!!!")
+}
+
+//qna6
+function clickcsix(){
+    document.getElementById("correctanswersix").style.backgroundColor = "green";
+    alert("Correct Answer!!!")
+}
+
+function clickwsix(){
+    document.getElementById("wronganswersix").style.backgroundColor = "red";
+    alert("Wrong Answer!!!")
+}
+
+//qna7
+function clickcseven(){
+    document.getElementById("correctanswerseven").style.backgroundColor = "green";
+    alert("Correct Answer!!!")
+}
+
+function clickwseven(){
+    document.getElementById("wronganswerseven").style.backgroundColor = "red";
+    alert("Wrong Answer!!!")
+}
